4b5b1ee80faf43626af6e2fb5591e6f3add74e0a
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-24 21:49:18 +0300

    constants and enums

---
+++ RoomieFinderInfrastructure/Constants/ModelConstants.cs
@@ -0,0 +1,27 @@
+﻿
+namespace RoomieFinderInfrastructure.Constants
+{
+    public static class ModelConstants
+    {
+        public static class ApplicationUserConstants
+        {
+            public const int FirstNameMaxLength = 50;
+            public const int FirstNameMinLength = 2;
+
+            public const int LastNameMaxLength = 60;
+            public const int LastNameMinLength = 2;
+        }
+
+        public static class UniversityConstants
+        {
+            public const int NameMaxLength= 100;
+            public const int NameMinLength = 5;
+        }
+
+        public static class DormitoryConstants
+        {
+            public const int NameMaxLength = 30;
+            public const int NameMinLength = 4;
+        }
+    }
+}
---
+++ RoomieFinderInfrastructure/Enums/RoomType.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace RoomieFinderInfrastructure.Enums
+{
+    public enum RoomType
+    {
+        Single = 1,
+        Duplex=2,
+        Apartament =3,
+    }
+}
