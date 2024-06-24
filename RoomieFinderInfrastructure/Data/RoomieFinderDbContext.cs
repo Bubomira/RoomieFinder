aa2ff1d6fc706e2746437e270c8e0ae988a49fb8
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-24 21:51:29 +0300

    db context

---
+++ RoomieFinderInfrastructure/Data/RoomieFinderDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using RoomieFinderInfrastructure.Models;
+
+
+namespace RoomieFinderInfrastructure.Data
+{
+    public class RoomieFinderDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public RoomieFinderDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<UniversityAdmin> UniversityAdmins { get; set; }
+        public DbSet<University> Universities { get; set; }      
+        public DbSet<Dormitory> Dormitories { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+    }
+}
