b3bfa1570e7e98ff006f3cf9071b0d2c9ea88f1c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-24 21:28:59 +0300

    sql server package  + extention of builder to add db context

---
+++ RoomieFinderAPI/Extentions/BuilderExtentions.cs
@@ -0,0 +1,21 @@
+﻿using RoomieFinderInfrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class BuilderExtentions
+{
+
+    public static IServiceCollection AttachDbContext(this IServiceCollection builder, IConfiguration config )
+    {
+
+        string connectionString = config.GetConnectionString("DefaultConnection")?? "";
+
+        builder.AddDbContext<RoomieFinderDbContext>(opt =>
+        {
+            opt.UseSqlServer(connectionString);
+        });
+
+        return builder;
+    }
+}
--- RoomieFinderAPI/Program.cs
+++ RoomieFinderAPI/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AttachDbContext(builder.Configuration);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
