ff91f4442beca18e67d0ac9d7acd121403bc541f
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-25 14:49:21 +0300

    added tables to db context

--- RoomieFinderInfrastructure/Data/RoomieFinderDbContext.cs
+++ RoomieFinderInfrastructure/Data/RoomieFinderDbContext.cs
@@ -8,7 +8,8 @@ namespace RoomieFinderInfrastructure.Data
 {
     public class RoomieFinderDbContext : IdentityDbContext<ApplicationUser>
     {
-        public RoomieFinderDbContext(DbContextOptions options):base(options)
+        public RoomieFinderDbContext(DbContextOptions<RoomieFinderDbContext> options)
+            :base(options)
         {
             
         }
@@ -18,6 +19,31 @@ namespace RoomieFinderInfrastructure.Data
         public DbSet<University> Universities { get; set; }      
         public DbSet<Dormitory> Dormitories { get; set; }
         public DbSet<Room> Rooms { get; set; }
+        public DbSet<Questionnaire> Questionnaires { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<StudentAnswer> StudentsAnswers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //many-tomany relationship between student and asnwers
+            builder.Entity<StudentAnswer>()
+                .HasOne(sa => sa.Student)
+                .WithMany(s => s.StudentAnswers)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasForeignKey(sa => sa.StudentId);
+
+            builder.Entity<StudentAnswer>()
+                .HasOne(sa => sa.Answer)
+                .WithMany(s => s.StudentAnswers)
+                .HasForeignKey(sa => sa.AnswerId);
+
+            //configuring key for many-to-many student answer table
+            builder.Entity<StudentAnswer>()
+                .HasKey(sa => new { sa.AnswerId, sa.StudentId });
+
+            base.OnModelCreating(builder);
+        }
 
     }
 }
