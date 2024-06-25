e0545d2c40bbdcc2c0b31bcb71b3b3d4179c8b7d
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-25 14:46:58 +0300

    added entity constants

--- RoomieFinderInfrastructure/Constants/ModelConstants.cs
+++ RoomieFinderInfrastructure/Constants/ModelConstants.cs
@@ -23,5 +23,26 @@ namespace RoomieFinderInfrastructure.Constants
             public const int NameMaxLength = 30;
             public const int NameMinLength = 4;
         }
+
+        public static class QuestionConstants
+        {
+            public const int TitleMaxLength = 200;
+            public const int TitleMinLength = 20;
+        }
+
+        public static class AnswerConstants
+        {
+            public const int ContentMaxLength = 100;
+            public const int ContentMinLength = 5;
+        }
+
+        public static class QuestionnaireConstants
+        {
+            public const int TitleMaxLength = 50;
+            public const int TitleMinLength = 3;
+
+            public const int DescriptionMaxLength = 300;
+            public const int DescriptionMinLength = 15;
+        }
     }
 }
