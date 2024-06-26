fb441d032bd40e395a2e5db7c3dbf0a60131202f
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-26 18:41:35 +0300

    moved student answer configuration in another file

---
+++ RoomieFinderInfrastructure/SeedDb/Configurations/StudentAnswerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RoomieFinderInfrastructure.Models;
+
+
+namespace RoomieFinderInfrastructure.SeedDb.Configurations
+{
+    internal class StudentAnswerConfiguration : IEntityTypeConfiguration<StudentAnswer>
+    {
+        public void Configure(EntityTypeBuilder<StudentAnswer> builder)
+        {
+            //many-tomany relationship between student and asnwers
+            builder
+                .HasOne(sa => sa.Student)
+                .WithMany(s => s.StudentAnswers)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasForeignKey(sa => sa.StudentId);
+
+            builder
+                .HasOne(sa => sa.Answer)
+                .WithMany(s => s.StudentAnswers)
+                .HasForeignKey(sa => sa.AnswerId);
+
+            //configuring key for many-to-many student answer table
+            builder.HasKey(sa => new { sa.AnswerId, sa.StudentId });
+        }
+    }
+}
