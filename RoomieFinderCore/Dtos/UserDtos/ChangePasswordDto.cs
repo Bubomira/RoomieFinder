27cda9cbfee03e879fd9059f17591ac5677acb6a
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-27 23:58:44 +0300

    new user dtos

---
+++ RoomieFinderCore/Dtos/UserDtos/ChangePasswordDto.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace RoomieFinderCore.Dtos.UserDtos
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string InitialPassword { get; set; }
+
+        [Required]
+        public required string NewPassword { get; set; }
+    }
+}
---
+++ RoomieFinderCore/Dtos/UserDtos/RegisterUserDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RoomieFinderCore.Dtos.UserDtos
+{
+    public class RegisterUserDto
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string SetUpPassword { get; set; }
+        public required int YearAtUniversity { get; set; }
+    }
+}
