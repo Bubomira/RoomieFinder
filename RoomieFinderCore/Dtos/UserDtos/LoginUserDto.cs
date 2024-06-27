8c352395bfb817a87f09d8939a5347e1b5751bc2
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-27 15:32:28 +0300

    LoginUserDto

---
+++ RoomieFinderCore/Dtos/UserDtos/LoginUserDto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace RoomieFinderCore.Dtos.UserDtos
+{
+    public class LoginUserDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
