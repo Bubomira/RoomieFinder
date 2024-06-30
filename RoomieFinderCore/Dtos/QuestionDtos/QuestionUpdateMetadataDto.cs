701b38e17fc6a0ee48a13fa5ce44a12233355e36
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-01 00:07:03 +0300

    question uodate dto

---
+++ RoomieFinderCore/Dtos/QuestionDtos/QuestionUpdateMetadataDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static RoomieFinderInfrastructure.Constants.ModelConstants.QuestionConstants;
+
+namespace RoomieFinderCore.Dtos.QuestionDtos
+{
+    public class QuestionUpdateMetadataDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public required string Title { get; set; }
+
+
+        [Required]
+        public required bool IsSingleAnswer { get; set; }
+    }
+}
