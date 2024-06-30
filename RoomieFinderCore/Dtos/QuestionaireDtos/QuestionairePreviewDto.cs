db59a62d859f98f766120fe5fd9d4f19046858b0
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-06-30 17:42:58 +0300

    new questionaire dtos

---
+++ RoomieFinderCore/Dtos/QuestionaireDtos/QuestionairePreviewDto.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace RoomieFinderCore.Dtos.QuestionaireDtos
+{
+    public class QuestionairePreviewDto
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+
+        public bool CanQuestionaireBeEdited { get; set; }
+
+    }
+}
