5490f439bbd6dd9c9ebd3bdc5aba1dc9036bc886
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-08 23:55:52 +0300

    added update/delete question from questionaire

--- RoomieFinderAPI/Controllers/QuestionController.cs
+++ RoomieFinderAPI/Controllers/QuestionController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RoomieFinderCore.Contracts.QuestionaireContracts;
 using RoomieFinderCore.Dtos.QuestionDtos;
+using RoomieFinderInfrastructure.Models;
 
 namespace RoomieFinderAPI.Controllers
 {
@@ -18,7 +19,7 @@ namespace RoomieFinderAPI.Controllers
             _questionaireContract = questionaireContract;
         }
 
-        [HttpPost("add/question/{questionaireId}")]
+        [HttpPost("add/to/questionaire/{questionaireId}")]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
@@ -27,10 +28,66 @@ namespace RoomieFinderAPI.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (await _questionaireContract.CheckIfQuestionaireExistsByIdAsync(questionaireId))
+                if (await _questionaireContract.CheckIfQuestionaireExistsByIdAsync(questionaireId) &&
+                    await _questionaireContract.CheckIfQuestionaireCanBeFilledOut(questionaireId))
                 {
                     await _questionContract.AddQuestionToQuestionaireAsync(questionaireId, questionPostDto);
                     return Created();
+                }
+                return NotFound();
+            }
+
+            return BadRequest();
+        }
+
+
+        [HttpPost("update/{questionId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(404)]
+        [Authorize(Roles = "GreatAdmin", AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> UpdateQuestionMetadata([FromBody] QuestionUpdateMetadataDto questionUpdateMetadataDto, int questionId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _questionContract.CheckIfQuestionExistsAsync(questionId) &&
+                    await _questionContract.CheckIfQuestionIsAttachedToQuestionaireAsync(questionId, questionUpdateMetadataDto.QuestionaireId))
+                {
+
+                    if (await _questionaireContract.CheckIfQuestionaireCanBeFilledOut(questionUpdateMetadataDto.QuestionaireId))
+                    {
+                        await _questionContract.UpdateQuestionMetadataAsync(questionId, questionUpdateMetadataDto);
+                        return NoContent();
+                    }
+                    return Forbid();
+                }
+                return NotFound();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("delete/{questionId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(404)]
+        [Authorize(Roles = "GreatAdmin", AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> DeleteQuestion(int questionId, [FromQuery] int questionaireId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _questionContract.CheckIfQuestionExistsAsync(questionId) &&
+                    await _questionContract.CheckIfQuestionIsAttachedToQuestionaireAsync(questionId, questionaireId))
+                {
+
+                    if (await _questionaireContract.CheckIfQuestionaireCanBeFilledOut(questionaireId))
+                    {
+                        await _questionContract.DeleteQuestionFromQuestionaireAsync(questionId);
+                        return NoContent();
+                    }
+                    return Forbid();
                 }
                 return NotFound();
             }
