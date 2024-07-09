4cbc550ca3543e2ed0f32cfd99cb431dc8c5c2bb
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-09 23:59:08 +0300

    .

--- RoomieFinderCore/Contracts/QuestionaireContracts/IQuestionContract.cs
+++ RoomieFinderCore/Contracts/QuestionaireContracts/IQuestionContract.cs
@@ -18,7 +18,7 @@ namespace RoomieFinderCore.Contracts.QuestionaireContracts
         /// <param name="questionId"></param>
         /// <param name="questionPostDto"></param>
         /// <returns></returns>
-        public Task UpdateQuestionMetadataAsync(int questionId, QuestionUpdateMetadataDto questionUpdateMetadataDto);
+        public Task UpdateQuestionMetadataAsync(int questionId, QuestionUpdateMetadataDto questionPostDto);
 
         /// <summary>
         /// Removes an existing question alongside with its answers from a questionaire
