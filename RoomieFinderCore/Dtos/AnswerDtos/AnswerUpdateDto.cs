07bb369f106329d33290ef6fd77423e97dbfb036
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-09 23:54:37 +0300

    answer update dto

---
+++ RoomieFinderCore/Dtos/AnswerDtos/AnswerUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace RoomieFinderCore.Dtos.AnswerDtos
+{
+    public class AnswerUpdateDto : AnswerPostDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public int QuestionaireId { get; set; }
+
+    }
+}
