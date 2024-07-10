baf442c44be97c0023c23ea2ed7f9fab05ac6b9d
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-10 23:54:04 +0300

    new answer dto

---
+++ RoomieFinderCore/Dtos/AnswerDtos/AnswerAttachDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomieFinderCore.Dtos.AnswerDtos
+{
+    public class AnswerAttachDto : AnswerPostDto
+    {
+        [Required]
+        public int QuestionId { get; set; }
+    }
+}
