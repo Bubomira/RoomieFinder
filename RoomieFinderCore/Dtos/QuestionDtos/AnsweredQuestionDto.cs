9fc90e7a8a9ad2bc269e6a50efbb28b66ba01a9e
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-12 23:29:20 +0300

    new answer dto

---
+++ RoomieFinderCore/Dtos/QuestionDtos/AnsweredQuestionDto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace RoomieFinderCore.Dtos.QuestionDtos
+{
+    public class AnsweredQuestionDto
+    {
+        public List<int> ChosenAnswersDto { get; set; }
+          = new List<int>();
+    }
+}
