bd5c95f893f87ebc4a1a23c15a8b1c097a54aaba
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-15 21:11:28 +0300

    added summary to dormitory contract

--- RoomieFinderCore/Contracts/DormitotyContracts/IDormitoryContract.cs
+++ RoomieFinderCore/Contracts/DormitotyContracts/IDormitoryContract.cs
@@ -6,7 +6,16 @@ namespace RoomieFinderCore.Contracts.DormitotyContracts
 {
     public interface IDormitoryContract
     {
+        /// <summary>
+        /// Gets all of the available dormitories in the database
+        /// </summary>
+        /// <returns></returns>
         public Task<List<DormitoryPreviewDto>> GetAllDormitoriesAsync();
+        /// <summary>
+        /// Checks if a dormitory exists by given id
+        /// </summary>
+        /// <param name="dormitoryId"></param>
+        /// <returns></returns>
         public Task<bool> CheckIfDormitoryExistsByIdAsync(int dormitoryId);
     }
 }
