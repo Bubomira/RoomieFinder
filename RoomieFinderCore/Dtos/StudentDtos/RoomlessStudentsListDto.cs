f5fb45d1c57438bb471e1c26c75b30c49b188e94
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-15 18:52:34 +0300

    new student dtos

---
+++ RoomieFinderCore/Dtos/StudentDtos/RoomlessStudentsListDto.cs
@@ -0,0 +1,27 @@
+﻿
+namespace RoomieFinderCore.Dtos.StudentDtos
+{
+    public class RoomlessStudentsListDto
+    {
+        private int pageNumber;
+        public const int StudentsOnPage = 12;
+
+        public int PageNumber
+        {
+            get => pageNumber; set
+            {
+                if (value <= 0)
+                {
+                    throw new FormatException();
+                }
+                pageNumber = value;
+            }
+        }
+
+        public int TotalCount { get; set; }
+
+        public List<StudentWithoutARoomDto> studentsWithoutARoom { get; set; }
+           = new List<StudentWithoutARoomDto>();
+
+    }
+}
---
+++ RoomieFinderCore/Dtos/StudentDtos/StudentWithoutARoomDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RoomieFinderCore.Dtos.StudentDtos
+{
+    public class StudentWithoutARoomDto
+    {
+        public required string FullName { get; set; }
+        public required int YearAtUniversity { get; set; }
+
+    }
+}
