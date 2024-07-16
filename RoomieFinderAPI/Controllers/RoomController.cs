7911685b9d1847b4ff8ffb37a058efbedce2582e
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-16 17:44:17 +0300

    add student to room action + room controller

---
+++ RoomieFinderAPI/Controllers/RoomController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RoomieFinderCore.Contracts.RoomContracts;
+
+namespace RoomieFinderAPI.Controllers
+{
+    [ApiController]
+    [Route("room")]
+    [Authorize(Roles = "GreatAdmin", AuthenticationSchemes = "Bearer")]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomCheckerContract _roomCheckerContract;
+        private readonly IRoomContract _roomContract;
+
+        public RoomController(IRoomCheckerContract roomCheckerContract,
+            IRoomContract roomContract)
+        {
+            _roomCheckerContract = roomCheckerContract;
+            _roomContract = roomContract;
+        }
+
+        [HttpGet("{roomId}/add/student/{userId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> AddStudentToRoom(int roomId, string userId)
+        {
+            if (await _roomCheckerContract.CheckIfRoomExistsByIdAsync(roomId))
+            {
+                if (!await _roomCheckerContract.CheckIfStudentIsAlreadyAsignedToTheSpecifiedRoomAsync(userId, roomId)
+                    && await _roomCheckerContract.CheckIfRoomHasCapacityAsync(roomId))
+                {
+                    await _roomContract.AsignRoomToStudentAsync(userId, roomId);
+                    return NoContent();
+                }
+                return BadRequest();
+            }
+
+            return NotFound();
+        }
+    }
+}
