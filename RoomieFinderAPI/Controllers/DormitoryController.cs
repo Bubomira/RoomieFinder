72335b70aaa36fca5f4caa900309faa7b917554c
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-16 13:29:22 +0300

    dormitory controller

---
+++ RoomieFinderAPI/Controllers/DormitoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RoomieFinderCore.Contracts.DormitotyContracts;
+using RoomieFinderCore.Dtos.DormitoryDtos;
+using RoomieFinderCore.Dtos.RoomDtos;
+
+namespace RoomieFinderAPI.Controllers
+{
+    [ApiController]
+    [Route("dormitory")]
+    [Authorize(Roles = "GreatAdmin", AuthenticationSchemes = "Bearer")]
+    public class DormitoryController : ControllerBase
+    {
+        private readonly IDormitoryContract _dormitoryContract;
+
+        public DormitoryController(IDormitoryContract dormitoryContract)
+        {
+            _dormitoryContract = dormitoryContract;
+        }
+
+        [HttpGet("all")]
+        [ProducesResponseType(200, Type = typeof(List<DormitoryPreviewDto>))]
+        public async Task<IActionResult> GetAllDormitories() =>
+            Ok(await _dormitoryContract.GetAllDormitoriesAsync());
+
+        [HttpGet("{dormitoryId}/rooms")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(List<RoomDetailsDto>))]
+        public async Task<IActionResult> GetAllRoomsFromADormitory(int dormitoryId)
+        {
+            if (await _dormitoryContract.CheckIfDormitoryExistsByIdAsync(dormitoryId))
+            {
+                return Ok(await _dormitoryContract.GetAllRoomsFromADormitoryByIdAsync(dormitoryId));
+            }
+            return NotFound();
+        }
+
+    }
+}
