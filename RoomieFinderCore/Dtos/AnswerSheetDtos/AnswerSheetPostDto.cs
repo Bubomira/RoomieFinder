623ffaa09b24ab9024bd64df9746e84abceb5550
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-21 17:23:45 +0300

    .

--- RoomieFinderCore/Dtos/AnswerSheetDtos/AnswerSheetPostDto.cs
+++ RoomieFinderCore/Dtos/AnswerSheetDtos/AnswerSheetPostDto.cs
@@ -1,14 +1,9 @@
 ﻿
-
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-
 namespace RoomieFinderCore.Dtos.AnswerSheetDtos
 {
     public class AnswerSheetPostDto : AnswerSheetMetadataDto
     {
-
-        public IList<int> QualityIds { get; set; } =
+        public List<int> QualityIds { get; set; } =
             new List<int>();
     }
 }
