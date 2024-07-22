b2ace6903caa2240f53bee683c9d2ddc9704ae9a
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-22 23:57:04 +0300

    user constants added to front end

---
+++ client/roomie-finder-client/src/app/utils/constants.ts
@@ -0,0 +1,14 @@
+export const userConstants ={
+         firstNameMaxLength:50,
+         firstNameMinLength:2,
+
+         lastNameMaxLength:60,
+         lastNameMinLength:2,
+
+         usernameMaxLength:30,
+         usernameMinLength:2,
+
+         passwordMaxLength:20,
+         passwordMinLength:8
+    }
+
