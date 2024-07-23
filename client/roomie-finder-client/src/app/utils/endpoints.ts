a085a525136fc3a8bb9929be17c3a218ee707bae
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-23 18:53:03 +0300

    added endpoints

---
+++ client/roomie-finder-client/src/app/utils/endpoints.ts
@@ -0,0 +1,16 @@
+let apiEndpoint ='https://localhost:7210';
+
+export const authenticationEndpoint = `${apiEndpoint}/auth`
+
+export const answerSheetEndpoint = `${apiEndpoint}/answerSheet`
+
+export const requestEndpoint = `${apiEndpoint}/request`
+
+export const dormitoryEndpoint = `${apiEndpoint}/dormitory`
+
+export const roomEndpoint = `${apiEndpoint}/room`
+
+export const schoolYearEndpoint = `${apiEndpoint}/school/year`
+
+export const studentEndpoint = `${apiEndpoint}/student`
+
