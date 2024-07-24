82eda5dcc2e7f05c5d5c465ef3240bafc9eeb39d
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-24 20:51:08 +0300

    change password component+added pattern checking for new password+ service

--- client/roomie-finder-client/src/app/components/auth/change-password/change-password.component.ts
+++ client/roomie-finder-client/src/app/components/auth/change-password/change-password.component.ts
@@ -1,8 +1,13 @@
 import { Component,inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { FormBuilder, Validators } from '@angular/forms';
 
 import { userConstants } from '../../../utils/constants';
+import { PasswordChange } from '../../../models/userModels';
+import { AuthenticationService } from '../../../services/authentication/authentication.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { JwtService } from '../../../services/jwt/jwt.service';
 
 @Component({
   selector: 'app-change-password',
@@ -10,6 +15,10 @@ import { userConstants } from '../../../utils/constants';
 })
 export class ChangePasswordComponent {
   private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthenticationService);
+  private jwtService = inject(JwtService)
+  private router = inject(Router);
+  
 
   changePasswordForm = this.formBuilder.group({
     email:['',[Validators.required,
@@ -20,7 +29,8 @@ export class ChangePasswordComponent {
     ]],
     newPassword:['',[Validators.required,
       Validators.maxLength(userConstants.passwordMaxLength),
-      Validators.minLength(userConstants.passwordMinLength)
+      Validators.minLength(userConstants.passwordMinLength),
+      Validators.pattern('([a-z][0-9])+|([0-9][a-z])+'),
     ]],
     repeatPassword:[''],
   })
@@ -35,7 +45,21 @@ export class ChangePasswordComponent {
         alert('Passwords must match!');
         return;
       }
-
+      let user : PasswordChange= {
+        email:this.c.email.value?this.c.email.value:'',
+        initialPassword:this.c.initialPassword.value? this.c.initialPassword.value:'',
+        newPassword:this.c.newPassword.value?this.c.newPassword.value:''
+      }
+      this.authService.changePasswordAsync(user).subscribe({
+        next:(res:string)=>{
+          this.jwtService.changeHasSetPassword();
+          return this.router.navigateByUrl('/');
+        },
+        error:(error:HttpErrorResponse)=>{
+          console.log(error)
+          alert(error.message);
+        }
+      })
 
   }
 
--- client/roomie-finder-client/src/app/services/authentication/authentication.service.ts
+++ client/roomie-finder-client/src/app/services/authentication/authentication.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoggedInUser, LoginUser } from '../../models/userModels';
+import { LoggedInUser, LoginUser,PasswordChange} from '../../models/userModels';
 import { Observable } from 'rxjs';
 
 import { authenticationEndpoint } from '../../utils/endpoints';
+import { JwtService } from '../jwt/jwt.service';
 
 
 @Injectable({
@@ -11,9 +12,18 @@ import { authenticationEndpoint } from '../../utils/endpoints';
 })
 export class AuthenticationService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private jwt:JwtService) { }
 
   loginUserAsync(user:LoginUser):Observable<LoggedInUser>{
     return this.http.post<LoggedInUser>(`${authenticationEndpoint}/login`,user);
+  }
+
+  changePasswordAsync(user:PasswordChange){
+    return this.http.post(`${authenticationEndpoint}/password/change`,user,{
+      headers:{
+        'Authorization':`Bearer ${this.jwt.getUserToken()}`
+      },
+      responseType:'text'
+    });
   }
 }
