2ab28d41167a656512800f8089c33783341f2498
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-26 18:47:44 +0300

    student register service + onSubmit function

--- client/roomie-finder-client/src/app/components/auth/register-student/register-student.component.ts
+++ client/roomie-finder-client/src/app/components/auth/register-student/register-student.component.ts
@@ -1,10 +1,12 @@
 import { Component,inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder,Validator, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 import { AuthenticationService } from '../../../services/authentication/authentication.service';
+import { RegisterUser } from '../../../models/userModels';
 
 import { userConstants,studentConstants } from '../../../utils/constants';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register-student',
@@ -33,15 +35,43 @@ export class RegisterStudentComponent {
     ]],
     setUpPassword:['',[Validators.required,
       Validators.maxLength(userConstants.passwordMaxLength),
-      Validators.minLength(userConstants.passwordMinLength)
+      Validators.minLength(userConstants.passwordMinLength),
+      Validators.pattern('([a-z][0-9])+|([0-9][a-z])+'),
     ]],
     yearAtUniversity:[1,[Validators.required,
       Validators.max(studentConstants.maxYearAtUniversity),
       Validators.min(studentConstants.minYearAtUniversity)
     ]],
-    isMale:[true,[Validators.required]]
+    isMale:['true',[Validators.required]]
   })
 
+  onSubmit(e:Event){
+    e.preventDefault();
+
+    if(this.registerForm.invalid){
+      return;
+    }
+
+    
+    let user:RegisterUser={
+    firstName:this.c.firstName.value? this.c.firstName.value:'',
+    lastName:this.c.lastName.value? this.c.lastName.value:'',
+    email:this.c.email.value? this.c.email.value:'',
+    username:this.c.username.value? this.c.username.value:'',
+    setUpPassword:this.c.setUpPassword.value? this.c.setUpPassword.value:'',
+    yearAtUniversity:this.c.yearAtUniversity.value? this.c.yearAtUniversity.value:1,
+    isMale:this.c.isMale.value=='true'?true:false
+   }
+   
+   this.authService.registerUser(user).subscribe({
+    next:(message:string)=>{
+      alert(message);
+      return this.router.navigate(['/'])
+    },
+    error:(error:HttpErrorResponse)=>alert('Unsuccessful registration')
+   })
+
+  }
 
 
   protected c = this.registerForm.controls;
--- client/roomie-finder-client/src/app/services/authentication/authentication.service.ts
+++ client/roomie-finder-client/src/app/services/authentication/authentication.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoggedInUser, LoginUser,PasswordChange} from '../../models/userModels';
+import { LoggedInUser, LoginUser,PasswordChange, RegisterUser} from '../../models/userModels';
 import { Observable } from 'rxjs';
 
 import { authenticationEndpoint } from '../../utils/endpoints';
@@ -16,6 +16,15 @@ export class AuthenticationService {
 
   loginUserAsync(user:LoginUser):Observable<LoggedInUser>{
     return this.http.post<LoggedInUser>(`${authenticationEndpoint}/login`,user);
+  }
+
+  registerUser(user:RegisterUser){
+    return this.http.post(`${authenticationEndpoint}/register`,user,{
+      headers:{
+        'Authorization':`Bearer ${this.jwt.getUserToken()}`
+     },
+     responseType:'text'
+    })
   }
 
   logoutAsync(){
