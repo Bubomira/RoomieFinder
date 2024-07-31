e2f49008ff4b68eab3a47c4ee869e86b35d6be17
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-07-31 13:35:08 +0300

    added url builder

---
+++ client/roomie-finder-client/src/app/utils/urlBuilder.ts
@@ -0,0 +1,16 @@
+import {buildUrl} from 'build-url-ts'
+
+import * as endpoints from  './endpoints'
+import { areGraduated, genderPreference } from './enums'
+
+export const buildStudentListUrl=
+     (pageNumber:number,search:string|null,graduated:areGraduated,gender:genderPreference)=>
+      buildUrl(endpoints.studentEndpoint,{
+         path:'all',
+         queryParams:{
+             pageNumber:pageNumber,
+             searchTerm:search,
+             areGraduated:graduated?graduated:areGraduated.doesntMatter,
+             genderPreference:gender?gender:genderPreference.doesntMatter
+         }
+       })
