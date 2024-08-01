1cccf1f7e33fef48bf4d08f8f245c03031c6d8c7
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-08-01 11:53:39 +0300

    added search form to student list

--- client/roomie-finder-client/src/app/components/student-list/student-list.component.ts
+++ client/roomie-finder-client/src/app/components/student-list/student-list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+
 import { StudentService } from '../../services/student/student.service';
 import { areGraduated, genderPreference } from '../../utils/enums';
-import { HttpErrorResponse } from '@angular/common/http';
 import { StudentSearchList } from '../../models/studentModels';
 
 @Component({
@@ -10,8 +12,17 @@ import { StudentSearchList } from '../../models/studentModels';
   templateUrl: './student-list.component.html',
 })
 export class StudentListComponent {
+  private formBuilder = inject(FormBuilder);
+  protected areGraduated = areGraduated;
+  protected genderPreference = genderPreference;
   protected pageNumber=0;
   protected studentList:any;
+
+  protected searchForm = this.formBuilder.group({
+    graduated:[areGraduated.doesntMatter],
+    searchTerm:[''],
+    gender:[genderPreference.doesntMatter]
+  })
 
    constructor(private activatedRoute:ActivatedRoute,private studentService:StudentService) {
        activatedRoute.queryParamMap.subscribe(params=>{
@@ -28,8 +39,12 @@ export class StudentListComponent {
            console.log(err);
          },
          
-       })
-       
+       })      
+   }
+
+   onSubmit(e:Event){
+      e.preventDefault();
+      console.log(this.searchForm)
    }
 
     
