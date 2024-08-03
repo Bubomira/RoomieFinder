b949af335c7710335a98b682c1b9b4dfc7099124
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-08-03 13:14:37 +0300

    request enums added

--- client/roomie-finder-client/src/app/utils/enums.ts
+++ client/roomie-finder-client/src/app/utils/enums.ts
@@ -9,3 +9,18 @@ export enum genderPreference{
     female=2,
     doesntMatter=3
 }
+
+export enum requestType{
+    singleRoom = 1,
+    changeRoom = 2,
+    specificRoomate = 3,
+    doesntMatter = 4
+}
+
+export enum requestStatus{
+    pending = 1,
+    declined = 2,
+    accepted = 3,
+    archived = 4,
+    doesntMatter = 5
+}
